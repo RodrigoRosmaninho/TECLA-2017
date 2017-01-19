423e84d6f20a44c3c28f21e9fb1020e301325000
Rodrigo Rosmaninho <quico.rosmaninho@gmail.com>
2017-01-19 21:07:18 +0000

    Create F-AnoBissexto.java

---
+++ F-AnoBissexto.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class AnoBissexto {
+
+    static int  year;
+    
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        year = in.nextInt();
+        
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 100 == 0  && year % 400 == 0) ){
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }       
+        
+    }
+    
+}
