6f8110273e7e2dfbecc47687aaa9b9c58d521f9b
Rodrigo Rosmaninho <quico.rosmaninho@gmail.com>
2017-01-22 17:10:05 +0000

    Create W-Exemplo1.java

---
+++ W-Exemplo1.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class a
+{
+	public static void main (String[] args) throws IOException
+	{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String s = in.readLine();
+		System.out.println(s);
+	}
+}
